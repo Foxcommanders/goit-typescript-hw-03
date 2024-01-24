dcf094858b503b8d20fbe88d475463ed944bbec4
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-01-24 15:41:42 +0200

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,47 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+class Key {
+    private signature: number = Math.floor(Math.random() * 10);
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    constructor(private key: Key) {}
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    public door: boolean = false;
+    private tenants: Person[] = [];
+  
+    constructor(public key: Key) {}
+  
+    public comeIn(person: Person): void {
+      if (this.door) this.tenants.push(person);
+    }
+  
+    public abstract openDoor(key: Key): void;
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (this.key.getSignature() === key.getSignature()) {
+        this.door = true;
+      }
+    }
+  }
+  
+  const key = new Key();
+  
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  
+  house.comeIn(person);
+  
+  export {};
